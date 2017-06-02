7601c69daff28d31ac198e59e83b89fd1d3371e8
Jenny McCargish <jennymccargish@gmail.com>
2017-06-02 10:11:23 -0400

    jenny

---
+++ virtualPetShelter.java
@@ -0,0 +1,9 @@
+
+public class virtualPetShelter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ virtualPetShelterApp.java
@@ -0,0 +1,9 @@
+
+public class virtualPetShelterApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
